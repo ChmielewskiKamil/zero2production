3c0af261536f0a4da17cbc75c1e3693f88d46184
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-17 22:57:33 +0200

    ♻️ refactor: move logic to library

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-/**
- * This is the handler
- * App is trying to match the HTTP verb
- * to a matching registered endpoint -->
- * handler is used
- */
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
+use zero2production::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            // this request is only passed to the handler
-            // if the method is GET
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
